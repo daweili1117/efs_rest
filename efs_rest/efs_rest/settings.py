bab518da9374e88d6f3078751dd86b4e5e9353d0
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2021-03-15 16:25:05 -0500

    Update settings.py

--- efs_rest/efs_rest/settings.py
+++ efs_rest/efs_rest/settings.py
@@ -132,7 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
